6d1d67a17a2678822aad919469e96ea881d3689b
JamesGotsell <j.e.gotsell@gmail.com>
2016-02-22 23:20:44 +0000

    working on the layout of review page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -12,5 +12,5 @@ module.exports.locationInfo = function(req, res) {
 /* get add review page*/
 
 module.exports.addReview = function(req, res){
-  res.render('index', {title: 'Add review'});
+  res.render('location-review-form', {title: 'Add review'});
 };
