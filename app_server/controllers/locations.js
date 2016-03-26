b7ed4ad00f1093b6da4313e1503fb6feecb37156
JamesGotsell <j.e.gotsell@gmail.com>
2016-03-26 00:37:04 +0000

    moved data to the controller, working on getting the data displaying in the view , problems with jade indentation

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /* GET 'home page */
 module.exports.homelist = function(req, res){
-    res.render('locations-list', {title: "Home"});
+    res.render('locations-list', {
+        title: "SportsBar  - find a sports club near you",
+        pageHeader: {
+            title: "SportsBar",
+            strapline: "fine a sports club near you!"
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        },{
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        },{
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
 };
 
 /* get location info page */
